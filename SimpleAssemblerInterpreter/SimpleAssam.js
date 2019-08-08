ce52d60fbcf835f358e0a54db6d1ba68eae0ab14
A <avilavate@live.in>
2019-08-08 15:18:06 +0530

    Added full instruction set

--- SimpleAssemblerInterpreter/SimpleAssam.js
+++ SimpleAssemblerInterpreter/SimpleAssam.js
@@ -2,22 +2,22 @@
 
 function simple_assembler(program) {
     let register;
-    program.forEach(instruction => {
-      register=  Interpreter(instruction, register);
-      console.log(register);
+    program.forEach((instruction, index) => {
+        register = Interpreter(instruction, register, program, index);
+        console.log(register);
     });
 
     /* return a dictionary with the registers */
-	return register;
+    return register;
 }
 
-function Interpreter(Instruction, register){
-   
-   let Step= Instruction.split(" ");
-    switch (Step[0]) { 
+function Interpreter(Instruction, register, program, index) {
+
+    let Step = Instruction.split(" ");
+    switch (Step[0]) {
         case 'mov':
-            let Steps=Instruction.split(" ");
-            register =Steps[2];
+            let Steps = Instruction.split(" ");
+            register = Steps[2];
             break;
         case 'inc':
             register++;
@@ -26,11 +26,14 @@ function Interpreter(Instruction, register){
             register--;
             break;
         case 'jnz':
-            break;        
+            let newSteps = Instruction.split(" ");
+            let newInstruction = program[index-newSteps[2]];
+            return Interpreter(newInstruction,register,program);
+            break;
         default:
             break;
     }
     return register;
 }
 
-console.log(simple_assembler(['mov a 5','inc a','dec a','dec a','jnz a -1','inc a']));
+console.log(simple_assembler(['mov a 5', 'inc a', 'dec a', 'dec a', 'jnz a -1', 'inc a']));
