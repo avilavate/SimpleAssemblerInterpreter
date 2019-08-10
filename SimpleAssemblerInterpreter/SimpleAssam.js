8a81d9b5c871c78ea4f3ed49a85e1a0264dfc0ae
Avil Avate <avilavate123@gmail.com>
2019-08-10 14:05:31 +0530

    Added symbol extractor

--- SimpleAssemblerInterpreter/SimpleAssam.js
+++ SimpleAssemblerInterpreter/SimpleAssam.js
@@ -1,10 +1,28 @@
 
+function lexicalSymbolExtractor(program) {
+    let symbols = [];
+    program.forEach(element => {
+        let instruction = element.split(" ");
+        if (instruction[0] === 'mov' && instruction.length == 3) {
+            if (instruction[1]) symbols.push(instruction[1]);
+            if (instruction[2] && Number.isNaN(new Number(instruction[2]))) symbols.push(instruction[2]);
+           
+        }
+        if (instruction[0] == 'inc' || instruction[0] === 'dec' && instruction.length == 2) {
+            symbols.push(instruction[1]);
+        
+        }
+    
+    });
+    let results= symbols.filter((item, index)=>{return symbols.indexOf(item)==index});
+    return results;
+}
 
 function simple_assembler(program) {
-    let register;
-    
+    let register = {};
+
     program.forEach((instruction, index) => {
-       
+
         register = Interpreter(instruction, register, program, index);
         // console.log(register);
     });
@@ -19,7 +37,7 @@ function Interpreter(Instruction, register, program, index) {
     switch (Step[0]) {
         case 'mov':
             let Steps = Instruction.split(" ");
-            register = Number.isInteger (new Number(Steps[2]))?Steps[2]:let Step[2] ;
+           // register = Number.isInteger(new Number(Steps[2])) ? Steps[2] : let Step[2];
             break;
         case 'inc':
             register++;
@@ -38,4 +56,6 @@ function Interpreter(Instruction, register, program, index) {
     return register;
 }
 
-console.log(simple_assembler(['mov a -10','mov b a','inc a','dec b','jnz a -2']));
+//console.log(simple_assembler(['mov a -10', 'mov b a', 'inc a', 'dec b', 'jnz a -2']));
+
+console.log(lexicalSymbolExtractor(['mov a -10', 'mov b a', 'inc a', 'dec b', 'jnz a -2']));
