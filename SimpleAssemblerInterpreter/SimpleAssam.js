24109dac08b54ab32c7c8511effd1c0655432730
Avil Avate <avilavate123@gmail.com>
2019-08-11 17:42:55 +0530

    Working on why code wars issue not resolved

--- SimpleAssemblerInterpreter/SimpleAssam.js
+++ SimpleAssemblerInterpreter/SimpleAssam.js
@@ -48,10 +48,12 @@ function Interpreter(Instruction, register, program, index) {
             // register = Number.isInteger(new Number(Steps[2])) ? Steps[2] : let Step[2];
             if (!isNaN(new Number(Steps[2])) && Object.keys(register).includes(Steps[1])) {
                 register[Steps[1]] = Steps[2];
+                
             }
             else
                 if (isNaN(new Number(Steps[2])) && Object.keys(register).includes(Steps[2]) && Object.keys(register).includes(Steps[1])) {
                     register[Steps[1]] = register[Steps[2]];
+                    var reset= Object.keys(register).includes(Steps[2])?register[Steps[2]]=0:null ;
                 }
             break;
         case 'inc':
@@ -77,6 +79,6 @@ function Interpreter(Instruction, register, program, index) {
 }
 
 console.log(simple_assembler(['mov a 5','inc a','dec a','dec a','jnz a -1', 'inc a']));
-
+console.log(simple_assembler(['mov a -10','mov b a','inc a','dec b','jnz a -2']));
 //console.log(lexicalSymbolExtractor(['mov a -10', 'mov b a', 'inc a', 'dec b', 'jnz a -2']));
 
