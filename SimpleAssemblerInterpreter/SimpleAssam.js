8fe8d93e4b2f19125910872483cd302834bfccb7
Avil Avate <avilavate123@gmail.com>
2019-08-11 13:40:08 +0530

    Fixed mov , dec, inc instructions

--- SimpleAssemblerInterpreter/SimpleAssam.js
+++ SimpleAssemblerInterpreter/SimpleAssam.js
@@ -6,30 +6,30 @@ function lexicalSymbolExtractor(program) {
         if (instruction[0] === 'mov' && instruction.length == 3) {
             if (instruction[1]) symbols.push(instruction[1]);
             if (instruction[2] && Number.isNaN(new Number(instruction[2]))) symbols.push(instruction[2]);
-           
+
         }
         if (instruction[0] == 'inc' || instruction[0] === 'dec' && instruction.length == 2) {
             symbols.push(instruction[1]);
-        
+
         }
-    
+
     });
-    let results= symbols.filter((item, index)=>{return symbols.indexOf(item)==index});
+    let results = symbols.filter((item, index) => { return symbols.indexOf(item) == index });
     return results;
 }
 
 function simple_assembler(program) {
-    let symbols=lexicalSymbolExtractor(program);
+    let symbols = lexicalSymbolExtractor(program);
     let register = {};
     symbols.forEach(element => {
-        register[element]=undefined;
+        register[element] = undefined;
     });
 
     program.forEach((instruction, index) => {
 
         let resultIntermediate = Interpreter(instruction, register, program, index);
-        Object.keys(resultIntermediate).forEach(key=>{
-            register[key]=resultIntermediate[key];
+        Object.keys(resultIntermediate).forEach(key => {
+            register[key] = resultIntermediate[key];
         });
         // console.log(register);
     });
@@ -40,28 +40,33 @@ function simple_assembler(program) {
 
 function Interpreter(Instruction, register, program, index) {
 
-    let Step = Instruction.split(" ");
-    switch (Step[0]) {
+    //let Step = Instruction.split(" ");
+    let Steps = Instruction.split(" ");
+    switch (Steps[0]) {
         case 'mov':
-            let Steps = Instruction.split(" ");
-           // register = Number.isInteger(new Number(Steps[2])) ? Steps[2] : let Step[2];
-           if( !isNaN(  new Number(Steps[2])) && Object.keys(register).includes(Steps[1])){
-               register[Steps[1]]= Steps[2];
-           }
-           else 
-           if(isNaN(new Number(Steps[2])) && Object.keys(register).includes(Steps[2]) && Object.keys(register).includes(Steps[1])){
-                register[Steps[1]]=register[Steps[2]];
-           }
+
+            // register = Number.isInteger(new Number(Steps[2])) ? Steps[2] : let Step[2];
+            if (!isNaN(new Number(Steps[2])) && Object.keys(register).includes(Steps[1])) {
+                register[Steps[1]] = Steps[2];
+            }
+            else
+                if (isNaN(new Number(Steps[2])) && Object.keys(register).includes(Steps[2]) && Object.keys(register).includes(Steps[1])) {
+                    register[Steps[1]] = register[Steps[2]];
+                }
             break;
         case 'inc':
-        //    register++;
+
+            if (Steps[1] && !Number.isNaN(Steps[1]) && Object.keys(register).includes(Steps[1]))
+                register[Steps[1]]++;
             break;
         case 'dec':
-         //   register--;
+
+            (Steps[1] && !Number.isNaN(Steps[1]) && Object.keys(register).includes(Steps[1]))
+            register[Steps[1]]-1;
             break;
         case 'jnz':
             let newSteps = Instruction.split(" ");
-            let newInstruction = newSteps[1] ? program[new Number(index) + new Number(newSteps[2])]:null;
+            let newInstruction = newSteps[1] ? program[new Number(index) + new Number(newSteps[2])] : null;
             return Interpreter(newInstruction, register, program);
             break;
         default:
