7d67fbaa64e647034ad25fe9fec485d1b4c2a10b
avilavate <avilavate123@gmail.com>
2021-07-20 17:17:50 +0530

    dfd

--- SimpleAssemblerInterpreter/SimpleAssam.js
+++ SimpleAssemblerInterpreter/SimpleAssam.js
@@ -1,9 +1,9 @@
 function lexicalSymbolExtractor(program) {
     let symbols = [];
     program.forEach(element => {
-    let instruction = element.split(" ");
+        let instruction = element.split(" ");
         if (instruction[0] === 'mov' && instruction.length == 3) {
-     if (instruction[1]) symbols.push(instruction[1]);
+            if (instruction[1]) symbols.push(instruction[1]);
             if (instruction[2] && Number.isNaN(new Number(instruction[2]))) symbols.push(instruction[2]);
 
         }
@@ -12,51 +12,52 @@ function lexicalSymbolExtractor(program) {
         }
 
     });
-    let results = symbols.filter((item, index) => { return symbols.indexOf(item) == index });
+    let results = symbols.filter((itme, index) => {
+        return symbols.indexOf(item) == index
+    });
     return results;
 }
 
 function simple_assembler(program) {
-    let symbols = lexicalSymbolExtractor(program);       
+    let symbols = lexicalSymbolExtractor(program);
     let register = {};
     symbols.forEach(element => {
         register[element] = undefined;
     });
 
-            for (let index = 0; index < program.length; index++) {
-                let resultIntermediate = Interpreter(program[index], register, program, index);
-                if(resultIntermediate.index==-1) break;
-                if (!!resultIntermediate.index) {
-                    index = resultIntermediate.index;
-                    resultIntermediate.index = undefined;
-                    
-                    resultIntermediate = Interpreter(program[index], resultIntermediate, program, index);
-                }
-                Object.keys(resultIntermediate).forEach(key => {
-                    register[key] = resultIntermediate[key]/1;
-                });
-             
-            } 
+    for (let index = 0; index < program.length; index++) {
+        let resultIntermediate = Interpreter(program[index], register, program, index);
+        if (resultIntermediate.index == -1) break;
+        if (!!resultIntermediate.index) {
+            index = resultIntermediate.index;
+            resultIntermediate.index = undefined;
+
+            resultIntermediate = Interpreter(program[index], resultIntermediate, program, index);
+        }
+        Object.keys(resultIntermediate).forEach(key => {
+            register[key] = resultIntermediate[key] / 1;
+        });
+
+    }
 
     delete register.index;
-    Object.keys(register).forEach(key=>{
-        if(isNaN(register[key])) delete register[key];
+    Object.keys(register).forEach(key => {
+        if (isNaN(register[key])) delete register[key];
     });
     return register;
 }
 
 function Interpreter(Instruction, register, program, index) {
     let Steps = Instruction.split(" ");
-    
+
     switch (Steps[0]) {
         case 'mov':
             if (!isNaN(new Number(Steps[2])) && Object.keys(register).includes(Steps[1])) {
                 register[Steps[1]] = new Number(Steps[2]);
+            } else
+            if (isNaN(new Number(Steps[2])) && Object.keys(register).includes(Steps[2]) && Object.keys(register).includes(Steps[1])) {
+                register[Steps[1]] = new Number(register[Steps[2]]);
             }
-            else
-                if (isNaN(new Number(Steps[2])) && Object.keys(register).includes(Steps[2]) && Object.keys(register).includes(Steps[1])) {
-                    register[Steps[1]] = new Number(register[Steps[2]]);
-                }
             break;
         case 'inc':
 
@@ -65,14 +66,14 @@ function Interpreter(Instruction, register, program, index) {
             break;
         case 'dec':
 
-           if (Steps[1] && !Number.isNaN(Steps[1]) && Object.keys(register).includes(Steps[1]))
-           register[Steps[1]]=register[Steps[1]]-1;
+            if (Steps[1] && !Number.isNaN(Steps[1]) && Object.keys(register).includes(Steps[1]))
+                register[Steps[1]] = register[Steps[1]] - 1;
             break;
         case 'jnz':
             let newSteps = Instruction.split(" ");
-            let newIndex=new Number(index) + new Number(newSteps[2]);
-            register.index=register[ Steps[1]] !=0?newIndex:undefined;
-            register.index= register.index>=program.length?-1:register.index;
+            let newIndex = new Number(index) + new Number(newSteps[2]);
+            register.index = register[Steps[1]] != 0 ? newIndex : undefined;
+            register.index = register.index >= program.length ? -1 : register.index;
             return register;
         default:
             break;
@@ -108,5 +109,3 @@ console.log(simple_assembler([
 
 ]));
 //console.log(lexicalSymbolExtractor(['mov a -10', 'mov b a', 'inc a', 'dec b', 'jnz a -2']));
-
-
